42f7cbf3ef2426d3089fd70523c0541046404ed6
Andy Guo <andy@withspoke.com>
2023-05-04 21:44:52 -0400

    ids

--- src/services/azureblob.tsx
+++ src/services/azureblob.tsx
@@ -22,7 +22,10 @@ const uploadFile = async (filePath: string, fileName: string) => {
 export const downloadFile = async () => {
   const blobServiceClient = new BlobServiceClient(
     process.env.REACT_APP_STORAGE!,
-    new InteractiveBrowserCredential()
+    new InteractiveBrowserCredential({
+      tenantId: "b0b48379-c90e-4c9d-b29a-9ac95ef4b3a7",
+      clientId: "bfcd6a7e-5e8c-4f98-b9da-013af0c31695",
+    })
   );
 
   const containerClient = blobServiceClient.getContainerClient("quotes");
