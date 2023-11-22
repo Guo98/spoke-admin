abde038f95ff5538389e705618bef444db586aa6
Andy Guo <andy@withspoke.com>
2023-11-22 11:20:02 -0500

    ops update

--- src/components/Operations/MarketplaceOrders/MarketRow.tsx
+++ src/components/Operations/MarketplaceOrders/MarketRow.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import {
   SelectChangeEvent,
@@ -65,6 +65,12 @@ const MarketRow = (props: RowProps) => {
   const [replace, setReplace] = useState(true);
   const [loading, setLoading] = useState(false);
   const [newReqEmail, setNewReqEmail] = useState(order.requestor_email);
+
+  useEffect(() => {
+    if (changeClient !== order.client) {
+      setClient(order.client);
+    }
+  }, [order.client]);
 
   const handleChange = (event: SelectChangeEvent) => {
     setStatus(event.target.value);
